3b37d8f8d8169193b35979766c67abc50d314c74
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-06-28 17:46:07 -0700

    Set up simple player and dealer views.

--- src/ui.rs
+++ src/ui.rs
@@ -21,6 +21,7 @@ pub fn ui(f: &mut Frame, app: &App) {
         ])
         .split(f.size());
 
+    // Title bar
     let title_block = Block::default()
         .borders(Borders::ALL)
         .style(Style::default());
@@ -33,9 +34,50 @@ pub fn ui(f: &mut Frame, app: &App) {
 
     f.render_widget(title, chunks[0]);
 
+    // Dealer view
+    let dealer_area = centered_rect(50, 75, chunks[2]);
+    let dealer_block = Block::default()
+        .title("Dealer")
+        .borders(Borders::ALL)
+        .style(Style::default().bg(Color::DarkGray));
+
+    f.render_widget(dealer_block, dealer_area);
+
+    // Player view
+    let player_area = centered_rect(50, 75, chunks[3]);
+    let player_block = Block::default()
+        .title("Player")
+        .borders(Borders::ALL)
+        .style(Style::default().bg(Color::DarkGray));
+
+    f.render_widget(player_block, player_area);
+
+    // Footer with allowed commands
     let command_footer = Paragraph::new("(h) to hit / (s) to stay / (q) to quit")
         .block(Block::default().borders(Borders::ALL));
 
     f.render_widget(command_footer, chunks[3]);
+}
 
+/// helper function to create a centered rect using up certain percentage of the available rect `r`
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
 }
