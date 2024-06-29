ec397e817fedb3ab5f2e96be8c504e9a215444c2
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-06-28 17:30:17 -0700

    Begin ui implementation with title and footer boxes.

--- src/ui.rs
+++ src/ui.rs
@@ -11,5 +11,31 @@ use ratatui::{
 use crate::app::App;
 
 pub fn ui(f: &mut Frame, app: &App) {
-    todo!()
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Min(2),
+            Constraint::Percentage(40),
+            Constraint::Percentage(40),
+            Constraint::Min(2),
+        ])
+        .split(f.size());
+
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let title = Paragraph::new(Text::styled(
+        "Command Line Blackjack",
+        Style::default().fg(Color::Green),
+    ))
+    .block(title_block);
+
+    f.render_widget(title, chunks[0]);
+
+    let command_footer = Paragraph::new("(h) to hit / (s) to stay / (q) to quit")
+        .block(Block::default().borders(Borders::ALL));
+
+    f.render_widget(command_footer, chunks[3]);
+
 }
