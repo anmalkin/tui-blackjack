b46a4606a967ae7f1ab2ea944a3b673fc93f90b6
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-06-29 17:12:20 -0700

    Set up app and terminal in main function. Add placeholder for new run_app function in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
 mod app;
-mod utils;
-mod ui;
-mod errors;
 mod cards;
+mod errors;
+mod ui;
+mod utils;
 
-use crate::app::run_game_loop;
+use std::{error::Error, io};
 
-fn main() {
-    run_game_loop();
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+use crate::app::App;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // set up terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = App::default();
+    let res = run_app(&mut app, &mut terminal);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{err:?}");
+    }
+
+    Ok(())
 }
 
+pub fn run_app<B: Backend>(app: &mut App, terminal: &mut Terminal<B>) -> io::Result<bool> {
+    todo!()
+}
